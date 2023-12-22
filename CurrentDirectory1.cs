32531e25ded994221c4d65f7c75230bd28f349c9
VeronikaSavenko <savenko.veronika1122@vu.cdu.edu.ua>
2023-12-22 08:45:11 +0200

    Добавьте файлы проекта.

---
+++ CurrentDirectory1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+namespace lb3._2
+{
+    public static class CurrentDirectory1
+    {
+        static Regex regexExtForImage = new Regex("^((.bmp)|(.gif)|(.tiff?)|(.jpe?g)|(.png))$", RegexOptions.IgnoreCase);
+
+        public static void CurrentFiles(string path, string[] data)
+        {
+            foreach (string fileName in data)
+            {
+                try
+                {
+                    string ext = Path.GetExtension(fileName);
+                    string newName = Path.ChangeExtension(fileName, null);
+                    newName += "-mirrored.gif";
+                    if (regexExtForImage.IsMatch(ext))
+                    {
+                        MirrorImage(newName, fileName);
+                        Console.WriteLine($" Вiдзеркалення картики {fileName} пройшло успiшно");
+                    }
+                    else { Console.WriteLine(fileName + " не є картинкою"); }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"Помилка при завантаженні зображення\n{fileName}\n", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        public static void MirrorImage(string name, string fileName)
+        {
+            Bitmap image = new Bitmap(fileName);
+            image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            image.Save(name, ImageFormat.Gif);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string way = @"D:\2k\lb3.2\photos\";
+            try
+            {
+                string[] data = Directory.GetFiles(way);
+                CurrentDirectory1.CurrentFiles(way, data);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine($"Папку {way} не знайдено");
+            }
+            Console.ReadKey();
+        }
+    }
+}
